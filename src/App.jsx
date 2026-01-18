5094ddc160dfe5d3f1bc39cfc6347db60e5213c1
TheHat1 <gfs.74.bg@gmail.com>
2026-01-18 18:15:57 +0200

    Add animated bg img at welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import WelcomePage from "./Pages/WelcomePage"
+import RecepiesPage from "./Pages/RecipesPage"
 
 function App() {
+  const navigate = useNavigate()
 
   return (
     <>
       <div className="w-screen h-37 bg-lime-900 z-10 flex items-center p-3 fixed top-0"> 
-        <img className="h-35" src="/images/nutri-bg-logo.png"/>
+        <img onClick={()=>{navigate('/')}} className="h-35 cursor-pointer" src="/images/nutri-bg-logo.png"/>
       </div>
       <Routes>
         <Route path="/" element={<WelcomePage/>}/>
+        <Route path="/recipes" element={<RecepiesPage/>}/>
       </Routes>
     </>
   )
--- src/Pages/WelcomePage.jsx
+++ src/Pages/WelcomePage.jsx
@@ -1,14 +1,40 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 export default function WelcomePage() {
+    const [img, setImg] = useState('')
+    const [CSS, setCSS] = useState('')
+    const br = useRef(0)
+    const imgs = ['/images/bg-img1.jpg','/images/bg-img2.jpg','/images/bg-img3.jpg']
     const footerRef = useRef()
     const navigate = useNavigate()
+
+    useEffect(() => {
+        setImg(imgs[Math.floor(Math.random() * 3)])
+
+        const interval = setInterval(() => {
+            setCSS("opacity-0 -translate-x-[5vw] duration-300")
+
+            setTimeout(() => {
+                br.current = (br.current + 1) % imgs.length
+                setImg(imgs[br.current])
+                setCSS("translate-x-[5vw] opacity-0 duration-[1ms]")
+
+                setTimeout(() => {
+                    setCSS("translate-x-0 opacity-100 duration-300")
+                }, 15)
+
+            }, 300)
+
+        }, 10000)
+
+        return () => clearInterval(interval)
+    }, [])
 
     return (
         <>
             <div className="fixed inset-0 -z-10">
-                <img className="w-screen h-screen object-cover pointer-events-none" src="/images/bg-img1.jpg" />
+                <img className={`w-screen h-screen object-cover pointer-events-none ${CSS}`} src={img} />
             </div>
             <div className="w-screen h-screen overflow-y-auto overflow-x-hidden pt-37">
                 <div className="w-screen h-[calc(100vh-var(--nav-height))] bg-emerald-900/40 backdrop-blur-xs flex justify-center items-center p-15">
@@ -16,10 +42,10 @@ export default function WelcomePage() {
                         <h1 className="text-5xl font-display text-white font-bold text-center">Здравето започва от храната.</h1>
                         <h1 className="text-5xl text-white caveat">Nutri BG</h1>
                         <div className="flex justify-between space-x-10 mt-6">
-                            <div onClick={()=>{
+                            <div onClick={() => {
                                 navigate('/recipes')
                             }}
-                             className="w-30 h-14 text-xl bg-emerald-950 rounded-lg text-white font-display font-bold flex justify-center items-center cursor-pointer hover:scale-110 hover:brightness-125 transition-all duration-150">Рецепти</div>
+                                className="w-30 h-14 text-xl bg-emerald-950 rounded-lg text-white font-display font-bold flex justify-center items-center cursor-pointer hover:scale-110 hover:brightness-125 transition-all duration-150">Рецепти</div>
                             <div onClick={() => {
                                 footerRef.current.scrollIntoView({
                                     behavior: "smooth",
