8e401625ce179b5975d6e7bd720615d50cffefb5
TheHat1 <gfs.74.bg@gmail.com>
2026-05-27 18:42:23 +0300

    commit2

---
+++ src/Components/RecipeCardSkeleton.jsx
@@ -0,0 +1,12 @@
+
+export default function RecipeCard() {
+
+    return (
+        <>
+            <div className="w-90 font-display h-135 rounded-lg shadow-2xl bg-lime-100 border transition-all hover:scale-103 cursor-pointer shrink-0 m-5 animate-pulse">
+                <div className="w-full h-85 rounded-t-lg shadow-2xl"></div>
+                <div className="flex border-t w-full justify-between items-center pr-3"></div>
+            </div>
+        </>
+    )
+}
